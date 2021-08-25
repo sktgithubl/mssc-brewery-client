c97391042aa3e73b9e49e2ee5ded90cafc3f4745
sktgithubl <skt.sipun123@gmail.com>
2021-08-25 22:18:31 +0530

    initial setup

---
+++ src/main/java/com/skt/msscbreweryclient/web/client/package-info.java
@@ -0,0 +1,1 @@
+package com.skt.msscbreweryclient.web.client;
---
+++ src/test/java/com/skt/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,15 @@
+package com.skt.msscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
